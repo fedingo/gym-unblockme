a4dd20a7608122e09862d681259111e2634f3d4b
fedingo <fedingo@gmail.com>
2019-04-12 15:35:03 +0200

    fixed typos

--- gym_unblockme/__init__.py
+++ gym_unblockme/__init__.py
@@ -9,17 +9,18 @@ register(id='UnblockMeListed-v0',
     entry_point='gym_unblockme.envs:UnblockMeEnvListed',
 )
 
-matrix_input = np.array([[0, 2, 2, 0],
+matrix_input = np.array([[0, 0, 2, 2],
                         [1, 1, 0, 3],
                         [0, 0, 0, 3],
-                        [0, 2, 2, 0]])
+                        [0, 0, 2, 2]])
 target_input = [1,3]
 
 register(id='UnblockMeFixedMap-v0',
          entry_point='gym_unblockme.envs:UnblockMeEnv',
          kwargs={'fix_map': True,
                  'matrix' : matrix_input,
-                 'goal'   : target_input})
+                 'goal'   : target_input,
+                 'max_steps': 100})
 
 register(id='UnblockMeListedFixedMap-v0',
          entry_point='gym_unblockme.envs:UnblockMeEnvListed',
@@ -33,4 +34,9 @@ register(id='UnblockMeCompactFixedMap-v0',
          kwargs={'fix_map': True,
                  'matrix' : matrix_input,
                  'goal'   : target_input,
-                 'listing': False})
+                 'listing': False,
+                 'max_steps': 100})
+
+register(id='UnblockMeCompact-v0',
+         entry_point='gym_unblockme.envs:UnblockMeEnvListed',
+         kwargs={'listing': False})
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='gym_unblockme',
       version='0.1',
-      url="https://github.com/tuzzer/gym-maze",
+      url="https://github.com/fedingo/gym-unblockme",
       author="Federico Rossetto",
       license="MIT",
       packages=["gym_unblockme", "gym_unblockme.envs"],
